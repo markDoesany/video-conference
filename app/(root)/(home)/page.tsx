3d9d4c80b0c6b81a9ebddd452e4de62a658251c4
Mark Teofel S. Cernal <20103214@usc.edu.ph>
2024-07-18 13:38:03 +0800

    added public folder and authentication

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+
+      </div>
+    </section>
+  )
+}
+
+export default Home
